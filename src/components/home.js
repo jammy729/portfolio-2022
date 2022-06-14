1642572f0af249c5b0494a272b5a2ce84bda3aaf
James Yoo <jamesyoo@Jamess-MacBook-Pro-3.local>
2022-06-14 12:22:41 -0700

    bio fix

--- src/components/home.js
+++ src/components/home.js
@@ -81,8 +81,8 @@ const Home = () => {
             University (SFU) majoring in BSc Interactive Arts and Technology
             with double concentration in Interactive Systems and Design. I am an 
             UX oriented front-end web and mobile developer mainly working with
-            React/React Native JS framework. I loves problem solving and designing
-            with code. When I am not coding, I loves to play and teach golf.
+            React/React Native JS framework. I love problem solving and designing
+            with code. When I am not coding, I play and teach golf.
           </Text>
         </Box>
       </Center>
